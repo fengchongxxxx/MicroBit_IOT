8287dbd5c583585283e5c99f5b90de000af64c53
fengchongxxxx <47021281+fengchongxxxx@users.noreply.github.com>
2019-02-25 14:24:12 +0800

    Updates.

--- test.ts
+++ test.ts
@@ -1,15 +1,15 @@
-Obloq.Obloq_mqtt_callback_user(function (message) {
+microIoT.Obloq_mqtt_callback_user(function (message) {
     basic.showString(message)
 })
-Obloq.Obloq_mqtt_setup(
+microIoT.Obloq_mqtt_setup(
     "yourSSID",
     "yourPASSWORD",
     "yourIotId",
     "yourIotPwd",
     "yourIotTopic",
-    Obloq.SERVERS.China
+    microIoT.SERVERS.China
 )
 basic.forever(() => {
-    Obloq.Obloq_mqtt_send_message("hello")
+    microIoT.Obloq_mqtt_send_message("hello")
     basic.pause(5000)
 })
